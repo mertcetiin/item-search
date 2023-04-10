d2bf73a089ac542fb9a26b8e4efcc6672c6283ba
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-04-10 19:39:05 +0300

    item-search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input, SimpleGrid } from '@mantine/core';
+import { useState } from 'react';
+import Movies from './components/Movies'
+
+const moviesItem = [
+  {
+    id: 1,
+    name: 'Cinema',
+    src: 'Cinemaa',
+  },
+  {
+    id: 2,
+    name: 'Directing',
+    src: 'Directingg',
+  },
+  {
+    id: 3,
+    name: 'Filmreel',
+    src: 'Filmreell',
+  },
+  {
+    id: 4,
+    name: 'Posters',
+    src: 'Posterss',
+  },
+  {
+    id: 5,
+    name: 'Projector',
+    src: 'Projectorr',
+  },
+  {
+    id: 6,
+    name: 'Theatre',
+    src: 'Theatree',
+  },
+];
 
 function App() {
+
+  let [searchValue, setSearchValue] = useState('');
+  let filteredItems = moviesItem.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input.Wrapper label="Search" className='search-bar'>
+        <Input onChange={(e) => setSearchValue(e.target.value)} />
+      </Input.Wrapper>
+      <br />
+
+      <SimpleGrid cols={3} className='container'>
+        {filteredItems.map(({ name, id, src }) => (
+          <div key={id}><Movies name={name} src={src} /></div>
+        ))}
+      </SimpleGrid>
+
+
     </div>
   );
 }
